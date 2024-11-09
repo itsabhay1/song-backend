859687a33bb78a14e937d7fb61542507a42414da
itsabhay1 <abhayag41@gmail.com>
2024-11-09 09:40:57 +0530

    callback link added

--- src/config/googleauth.js
+++ src/config/googleauth.js
@@ -21,7 +21,7 @@ const generateTokens = async (user) => {
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
+  callbackURL: "https://song-backend-bice.vercel.app/auth/google/callback"
 },
   async (accessToken, refreshToken, profile, cb) => {
     try {
